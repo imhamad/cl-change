4bd7496772aeb7ab47dcabb4e4c4f48c439620fd
Hamad <i.hassan.ext@rmh-media.com>
2020-03-07 00:08:01 +0500

    [counter]

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,37 @@
-alert('test');
+// /* Our JavaScript for Counters, Design task workflow i will set for Mustafa later 
+
+(function ($){
+    $.fn.counter = function() {
+      const $this = $(this),
+      numberFrom = parseInt($this.attr('data-from')),
+      numberTo = parseInt($this.attr('data-to')),
+      delta = numberTo - numberFrom,
+      deltaPositive = delta > 0 ? 1 : 0,
+      time = parseInt($this.attr('data-time')),
+      changeTime = 10;
+      
+      let currentNumber = numberFrom,
+      value = delta*changeTime/time;
+      var interval1;
+      const changeNumber = () => {
+        currentNumber += value;
+        (deltaPositive && currentNumber >= numberTo) || (!deltaPositive &&currentNumber<= numberTo) ? currentNumber=numberTo : currentNumber;
+        this.text(parseInt(currentNumber));
+        currentNumber == numberTo ? clearInterval(interval1) : currentNumber;  
+      }
+  
+      interval1 = setInterval(changeNumber,changeTime);
+    }
+  }(jQuery));
+  
+  $(document).ready(function(){
+  
+    $('.count-up').counter();
+
+    
+    new WOW().init();
+    
+    setTimeout(function () {
+      $('.count5').counter();
+    }, 3000);
+  });
